35eebc5918ce51bed2bb8a8383fd1dde2ddaa6cd
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-07-12 15:26:04 +0530

    motion

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -4,22 +4,31 @@ import { HiArrowCircleRight } from 'react-icons/hi';
 import Stribe from '../Assets/line-32.svg';
 import Arrow53 from '../Assets/arrow-53.svg';
 
+import { motion } from 'framer-motion';
 function LandingPage() {
   return (
     <div className="min-h-screen bg-blue-700 text-slate-200">
       <div>
         <div className="relative mx-auto mb-4 flex justify-center ">
-          <h1 className="z-10 pt-12 text-center text-7xl font-bold tracking-wider text-slate-100">
+          <motion.h1
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+            className="z-10 pt-12 text-center text-7xl font-bold tracking-wider text-slate-100"
+          >
             Welcome to GloBill
-          </h1>
-          <img
+          </motion.h1>
+          <motion.img
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 2 }}
             src={Stribe}
             alt="Stribe-line"
             className="right-30 absolute  -top-24 h-96 w-96"
           />
         </div>
         <h2 className=" mt-12  text-center text-3xl font-medium">
-          Powerful Billing Software for Your Business
+          Powerful Billing Application for Your Business
         </h2>
         <p className="z-50 pt-3 text-center font-semibold md:text-xl">
           Simplify your invoicing process, track payments effortlessly, and
@@ -28,7 +37,10 @@ function LandingPage() {
       </div>
 
       <div className="flex flex-col items-center justify-center md:flex-row">
-        <img
+        <motion.img
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
           src={BusinessSvg}
           alt="Globill-business-operations"
           className="flex-0 h-1/2 w-1/2"
